6b296929d9aeeb5a2784c8ac3b0fad00bf67bc84
Paulos256 <jimmyq139@yahoo.com>
2021-10-30 23:21:00 -0500

    Add vifm config

--- make_links.bat
+++ make_links.bat
@@ -5,7 +5,7 @@ set APP_DATA=%UP_PAUL%\AppData
 mklink /D "%APP_DATA%\Local\nvim" "%DOT_FILES_DIR%\.config\nvim"
 mklink "%UP_PAUL%\.vimrc" "%DOT_FILES_DIR%\.vimrc"
 
-mklink /D "%APP_DATA%\Vifm" "%DOT_FILES_DIR%\.config\vifm"
+mklink /D "%APP_DATA%\Roaming\Vifm" "%DOT_FILES_DIR%\.config\vifm"
 
 mklink "%UP_PAUL%\.gitconfig" "%DOT_FILES_DIR%\.gitconfig"
 mklink "%UP_PAUL%\.kdiff3rc" "%DOT_FILES_DIR%\.kdiff3rc"
