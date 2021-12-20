8f37aaa3f054d189395a51a4d210e485b06a7be3
Paulos256 <jimmyq139@yahoo.com>
2021-12-20 09:29:57 -0600

    Add bug.n Config first draft

--- make_links.bat
+++ make_links.bat
@@ -4,9 +4,8 @@ set APP_DATA=%UP_PAUL%\AppData
 
 mklink /D "%APP_DATA%\Local\nvim" "%DOT_FILES_DIR%\.config\nvim"
 mklink "%UP_PAUL%\.vimrc" "%DOT_FILES_DIR%\.vimrc"
-
 mklink /D "%APP_DATA%\Roaming\Vifm" "%DOT_FILES_DIR%\.config\vifm"
-
+mklink "%APP_DATA%\Roaming\bug.n\Config.ini" "%DOT_FILES_DIR%\bug.n\Config.ini"
 mklink "%UP_PAUL%\.gitconfig" "%DOT_FILES_DIR%\.gitconfig"
 
 pause
